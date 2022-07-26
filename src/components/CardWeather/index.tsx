21196cb82b10ad3c9a74c8295400cfc824adefbe
fahmi.yakub.flip <fahmi.yakub@flip.id>
2022-07-26 10:20:57 +0700

    create card

--- src/components/CardWeather/index.tsx
+++ src/components/CardWeather/index.tsx
@@ -1,16 +1,21 @@
-import {Image, Text, View} from 'react-native';
+import {Image, Text, View, TouchableOpacity} from 'react-native';
 import React, {memo} from 'react';
 import styles from './styles';
 import Config from 'react-native-config';
 
 const CardWeather = () => {
   return (
-    <View style={styles.container}>
+    <TouchableOpacity style={styles.container}>
       <Image
         source={{uri: Config.IMAGE_URL + '10d@2x.png'}}
         style={styles.icon}
       />
-    </View>
+      <View style={styles.descriptionContainer}>
+        <Text style={styles.textDate}>Tue, Jul 19, 2022 4:00 PM</Text>
+        <Text style={styles.textMain}>Clouds</Text>
+        <Text style={styles.textTemp}>Temp: 32.1°C</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
--- src/components/CardWeather/styles.ts
+++ src/components/CardWeather/styles.ts
@@ -5,14 +5,31 @@ import {hp, wp} from 'utils/StyleUtil';
 const styles = StyleSheet.create({
   container: {
     backgroundColor: Colors.white,
-    paddingLeft: wp(5),
+    paddingLeft: wp(3),
     borderBottomWidth: 2,
     borderBottomColor: Colors.light_grey,
     paddingVertical: hp(2),
+    flexDirection: 'row',
   },
   icon: {
-    width: wp(5),
-    height: wp(5),
+    width: wp(20),
+    height: wp(20),
+  },
+  descriptionContainer: {
+    left: wp(2),
+  },
+  textDate: {
+    color: Colors.black,
+    fontSize: hp(2),
+    fontWeight: 'bold',
+  },
+  textMain: {
+    color: Colors.black,
+    fontSize: hp(2),
+  },
+  textTemp: {
+    color: Colors.black,
+    fontSize: hp(1.5),
   },
 });
 
